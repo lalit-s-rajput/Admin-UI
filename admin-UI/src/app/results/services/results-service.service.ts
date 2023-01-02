000f6564aaa2bba0fc19e14e8d1fb398a6bd8e87
gayatri-l-rajput <rajputlalit591@gmail.com>
2023-01-02 20:40:29 +0530

    fix: error handling added

--- admin-UI/src/app/results/services/results-service.service.ts
+++ admin-UI/src/app/results/services/results-service.service.ts
@@ -1,51 +1,59 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
-import {Results} from '../../core/interface/results';
+import { BehaviorSubject, catchError, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Results } from '../../core/interface/results';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ResultService {
-  dataUrl = 'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json';
+  dataUrl =
+    'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json12';
   membersList = new BehaviorSubject<Results[]>([]);
   filteredList = new BehaviorSubject<Results[]>([]);
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  getData(){
-    if(!this.filteredList.value.length){
-      this.httpClient.get<Results[]>(this.dataUrl).subscribe((data)=>{
-        this.membersList.next(data);
-        this.filteredList.next(data);
+  getData() {
+    if (!this.filteredList.value.length) {
+      this.httpClient.get<Results[]>(this.dataUrl).subscribe({
+        next: (data) => {
+          this.membersList.next(data);
+          this.filteredList.next(data);
+        },
+        error: (err) => {
+          console.log(err);
+          throwError(() => new Error('test'))
+        },
       });
     }
     return this.filteredList;
   }
 
-  deleteMember(items:string[]){
-    let newMemberList = this.filteredList.value.filter((member)=>{
-       return !items.includes(member.id);
+  deleteMember(items: string[]) {
+    let newMemberList = this.filteredList.value.filter((member) => {
+      return !items.includes(member.id);
     });
-    // let newMemberList = this.filteredList.value.filter((member)=>{
-    //   return member.id!==item.id;
-    // });
     this.filteredList.next(newMemberList);
   }
 
-  editMember(item:Results){
-    let itemIndex = this.filteredList.value.findIndex((obj)=>{
+  editMember(item: Results) {
+    let itemIndex = this.filteredList.value.findIndex((obj) => {
       return obj.id == item.id;
     });
     this.filteredList.value[itemIndex] = item;
     this.filteredList.next(this.filteredList.value);
   }
-  filterData(key:string){
-    if(key.length){
-      let newData = this.membersList.value.filter((member)=>{
-        return member.email.toLowerCase().includes(key) || member.name.toLowerCase().includes(key) || member.role.toLowerCase().includes(key);
+  filterData(key: string) {
+    if (key.length) {
+      let newData = this.membersList.value.filter((member) => {
+        return (
+          member.email.toLowerCase().includes(key) ||
+          member.name.toLowerCase().includes(key) ||
+          member.role.toLowerCase().includes(key)
+        );
       });
       this.filteredList.next(newData);
-    }else{
+    } else {
       this.filteredList.next(this.membersList.value);
     }
   }
